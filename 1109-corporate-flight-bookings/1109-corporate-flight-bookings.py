d1f7ac5d8dc8fd61fc9f4ad19eac0c6bc34bb75b
YESHWANTH REDDY PEDDIREDDY <24b11ds163@adityauniversity.in>
2026-08-25 14:55:30 +0530

    Time: 28 ms (36.18%), Space: 31 MB (32%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,9 +1,9 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        ans = [0]*(n+2)
+        ans = [0]*(n+1)
         for i in bookings:
-            ans[i[0]] += i[2]
-            ans[i[1]+1] -= i[2]
+            ans[i[0]-1] += i[2]
+            ans[i[1]] -= i[2]
         for i in range(1,n+1):
             ans[i] += ans[i-1]
-        return ans[1:n+1]
+        return ans[0:n]
